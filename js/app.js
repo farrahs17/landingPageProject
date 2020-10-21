4434a0f0de65bdda693f0b7d4faa7de0fea249b3
farrahs17 <farrah.shawky.95@gmail.com>
2020-10-21 21:49:39 +0200

    Adding comments to code

--- js/app.js
+++ js/app.js
@@ -1,37 +1,45 @@
 
-
+// Creating the nav dynamically
 createNav = () => {
-    const sections = document.querySelectorAll('section');  
+    // Getting all the sections
+    const sections = document.querySelectorAll('section'); 
+    // Getting the ul that will have the dynamically added li 
     const list = document.querySelector('ul');
+    // Creating the document fragment that will have all the li
     const elements= document.createDocumentFragment(); 
 
-    for (let i = 0; i < sections.length; i++) {
+    // Looping over the sections
+    sections.forEach(section => {
         const li = document.createElement('li');
         const a = document.createElement('a');
-        a.innerText = sections[i].getAttribute('data-nav');
+        // Getting the anchor text from the sections data-nav attribute
+        a.innerText = section.getAttribute('data-nav');
         li.appendChild(a);
-        a.setAttribute('id',sections[i].getAttribute('id')+'a')
-
+        // Giving each anchor an ID attribute to be used for the activeState function
+        a.setAttribute('id', section.getAttribute('id')+'a')
+        // Adding the event listener to scroll to the appropriate section
         a.addEventListener('click', (e) => {
-            sections[i].scrollIntoView({behavior:'smooth'});
+            section.scrollIntoView({behavior:'smooth'});
         });
 
         elements.appendChild(li);
-    }
+    });
 
     list.appendChild(elements);
-
 }
 
+// Using IntersectionObserver to highlight the appropriate section while scrolling
 activeState = () => {
-
     let observerEnter = new IntersectionObserver((entries, observer) => {
         entries.forEach(entry => {
+            // Getting the appropriate anchor using the ID attached earlier
             const a = document.querySelector('#' + entry.target.getAttribute('id')+"a");
             if (entry.isIntersecting) {
+                // Adding the active class if it's intersecting the threshold
                 entry.target.classList.add('active-section');
                 a.classList.add('active-a');
             } else {
+                // Removing active class when not in threshold
                 entry.target.classList.remove('active-section');
                 a.classList.remove('active-a');
             }
@@ -40,6 +48,6 @@ activeState = () => {
 
     document.querySelectorAll('section').forEach(section => { observerEnter.observe(section) });
 }
-
+// Calling functions
 createNav();
 activeState();
